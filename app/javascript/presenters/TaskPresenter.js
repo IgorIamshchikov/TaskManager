56bfdcdd14b156e5d33b9374c1c650ea4414b27d
Igor Iamshchikov <iamshchikov.is@gmail.com>
2022-11-09 23:12:59 +0600

    Created Task and User Presenters

---
+++ app/javascript/presenters/TaskPresenter.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+import PropTypesPresenter from 'utils/PropTypesPresenter';
+
+export default new PropTypesPresenter(
+  {
+    id: PropTypes.number,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    author: PropTypes.shape(),
+    assignee: PropTypes.shape(),
+    transition: PropTypes.arrayOf(PropTypes.shape()),
+    state: PropTypes.string,
+  },
+  {},
+);
